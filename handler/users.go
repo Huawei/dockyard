6b7af4e4c3403f2a39ee7c43792a7e9a02a76633
MabinGo <bin.ma@huawei.com>
2015-06-04 21:22:45 +0800

    Support v1 ping & login
    Signed-off-by: MabinGo <bin.ma@huawei.com>

--- handler/users.go
+++ handler/users.go
@@ -2,19 +2,46 @@ package handler
 
 import (
 	"encoding/json"
-	"net/http"
-
+	"fmt"
 	"github.com/Unknwon/macaron"
+	"github.com/containerops/dockyard/models"
+	"github.com/containerops/dockyard/utils"
+	"net/http"
 )
 
 func GetUsersV1Handler(ctx *macaron.Context) (int, []byte) {
-	result, _ := json.Marshal(map[string]string{"message": ""})
+	TmpPrepare(ctx)
 
-	return http.StatusOK, result
+	if username, passwd, err := utils.DecodeBasicAuth(ctx.Req.Header.Get("Authorization")); err != nil {
+		fmt.Printf("[REGISTRY API V1] Decode Basic Auth Error:%v", err.Error())
+
+		result, _ := json.Marshal(map[string]string{"error": "Decode authorization failure"})
+		return http.StatusUnauthorized, result
+	} else {
+		user := new(models.User)
+		if err := user.Get(username, passwd); err != nil {
+			fmt.Printf("[REGISTRY API V1] Search user error: %v", err.Error())
+
+			result, _ := json.Marshal(map[string]string{"error": "User authorization failure"})
+			return http.StatusUnauthorized, result
+		}
+
+		fmt.Printf("[REGISTRY API V1] %v authorization successfully", username)
+		/*
+			memo, _ := json.Marshal(this.Ctx.Input.Header)
+			if err := user.Log(models.ACTION_SIGNUP, models.LEVELINFORMATIONAL, models.TYPE_APIV1, user.UUID, memo); err != nil {
+				fmt.Printf("[REGISTRY API V1] Log Erro:", err.Error())
+			}
+		*/
+
+		result, _ := json.Marshal(map[string]string{"status": "User authorization successfully"})
+		return http.StatusOK, result
+	}
 }
 
 func PostUsersV1Handler(ctx *macaron.Context) (int, []byte) {
-	result, _ := json.Marshal(map[string]string{"message": ""})
+	TmpPrepare(ctx)
 
-	return http.StatusOK, result
+	result, _ := json.Marshal(map[string]string{"message": ""})
+	return http.StatusUnauthorized, result
 }
