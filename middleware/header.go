acc3a312ca135a960278f94f29fed8afe5c5f485
root <maquanyi@huawei.com>
2015-08-04 22:53:31 +0800

    Use wrench setting of registry/distribution/standalone value, and update the runtime.conf example in the README.md

--- middleware/header.go
+++ middleware/header.go
@@ -13,12 +13,12 @@ func setRespHeaders() macaron.Handler {
 	return func(ctx *macaron.Context) {
 		if flag := strings.Contains(ctx.Req.RequestURI, "v1"); flag == true {
 			ctx.Resp.Header().Set("Content-Type", "application/json")
-			ctx.Resp.Header().Set("X-Docker-Registry-Standalone", "True")       //Standalone
-			ctx.Resp.Header().Set("X-Docker-Registry-Version", setting.Version) //Version
-			ctx.Resp.Header().Set("X-Docker-Registry-Config", setting.RunMode)  //Config
+			ctx.Resp.Header().Set("X-Docker-Registry-Standalone", setting.Standalone)   //Standalone
+			ctx.Resp.Header().Set("X-Docker-Registry-Version", setting.RegistryVersion) //Version
+			ctx.Resp.Header().Set("X-Docker-Registry-Config", setting.RunMode)          //Config
 		} else {
 			ctx.Resp.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			ctx.Resp.Header().Set("Docker-Distribution-Api-Version", "registry/2.0")
+			ctx.Resp.Header().Set("Docker-Distribution-Api-Version", setting.DistributionVersion)
 		}
 	}
 }
