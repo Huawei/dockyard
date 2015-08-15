fd999e547eddec20cf7482c8bff1fd49a5b78afc
MabinGo <bin.ma@huawei.com>
2015-08-15 17:16:32 +0800

    Support Docker 1.8 version

--- router/router.go
+++ router/router.go
@@ -37,6 +37,7 @@ func SetRouters(m *macaron.Macaron) {
 		m.Get("/", handler.GetPingV2Handler)
 		m.Head("/:namespace/:repository/blobs/:digest", handler.HeadBlobsV2Handler)
 		m.Post("/:namespace/:repository/blobs/uploads", handler.PostBlobsV2Handler)
+		m.Patch("/:namespace/:repository/blobs/uploads/:uuid", handler.PatchBlobsV2Handler)
 		m.Put("/:namespace/:repository/blobs/uploads/:uuid", handler.PutBlobsV2Handler)
 		m.Get("/:namespace/:repository/blobs/:digest", handler.GetBlobsV2Handler)
 		m.Put("/:namespace/:repository/manifests/:tag", handler.PutManifestsV2Handler)
