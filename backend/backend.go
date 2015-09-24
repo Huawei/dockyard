7828311db1af123ed6edd88bd3b29b5746c0c52e
MabinGo <bin.ma@huawei.com>
2015-09-24 19:17:11 +0800

    Init native driver file of backend
    Signed-off-by: MabinGo <bin.ma@huawei.com>

--- backend/backend.go
+++ backend/backend.go
@@ -2,6 +2,7 @@ package backend
 
 import (
 	"github.com/containerops/dockyard/backend/drivers"
+	_ "github.com/containerops/dockyard/backend/drivers/native"
 	_ "github.com/containerops/dockyard/backend/drivers/qiniu"
 	_ "github.com/containerops/dockyard/backend/drivers/upyun"
 )
