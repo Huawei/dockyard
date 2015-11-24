852474569a11d927342bd70d752d4a6d576f1639
osguydch <27235294@qq.com>
2015-11-24 19:39:34 +0800

    mazone s3 save file test ok.

--- backend/drivers/amazons3/amazons3_test.go
+++ backend/drivers/amazons3/amazons3_test.go
@@ -2,12 +2,18 @@ package backend
 
 import (
 	"testing"
+	
+	"github.com/containerops/wrench/setting"
 )
 
 func Test_amazons3save(t *testing.T) {
-
+	var url string
+	
+	
+	err := setting.SetConfig("../../../conf/containerops.conf")
+	
 	file := "amazons3_test.go"
-	url, err := amazons3save(file)
+	url, err = amazons3save(file)
 	if err != nil {
 		t.Error(err)
 		return
