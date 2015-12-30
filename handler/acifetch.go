5440a82e9346b1c0b0efdd3a7a43ddf016deb742
Zhouhu Peng <pengzhouhu@huawei.com>
2015-12-30 18:55:04 +0800

    add appc format image store

--- handler/acifetch.go
+++ handler/acifetch.go
@@ -4,50 +4,85 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/astaxie/beego/logs"
 	"gopkg.in/macaron.v1"
 
-	"github.com/containerops/wrench/setting"
+	"github.com/containerops/dockyard/models"
 )
 
-/* TBD:
-current implementation as blow just be added for testing ACI fetch,
-they would be updated after ACI ac-push finished
-*/
-
 func GetPubkeysHandler(ctx *macaron.Context, log *logs.BeeLogger) (int, []byte) {
-	var pubkey []byte
-	var err error
+	namespace := ctx.Params(":namespace")
 
-	pubkeypath := setting.ImagePath + "/acpool/" + "pubkeys.gpg"
-	if pubkey, err = ioutil.ReadFile(pubkeypath); err != nil {
-		// TBD: consider to fetch pubkey from other storage medium
+	r := new(models.AciRepository)
+	if err := r.GetRepository(namespace); err != nil {
+		log.Error("[ACI API] Get user %v details failed: %v", namespace, err.Error())
 
-		log.Error("[ACI API] Get pubkey file failed: %v", err.Error())
-		result, _ := json.Marshal(map[string]string{"message": "Get pubkey file failed"})
+		result, _ := json.Marshal(map[string]string{"message": "Get user details failed"})
+		return http.StatusNotFound, result
+	}
+    
+	files, err := ioutil.ReadDir(r.PubKeysPath)
+	if err != nil {
+		log.Error("[ACI API] Search pubkey file failed: %v", err.Error())
+
+		result, _ := json.Marshal(map[string]string{"message": "Search pubkey file failed"})
 		return http.StatusNotFound, result
 	}
 
+	// TODO: considering that one user has multiple pubkeys
+    var pubkey []byte
+    if len(files) > 0 {
+		filename := r.PubKeysPath + "/" + files[0].Name()
+		pubkey, err = ioutil.ReadFile(filename)
+		if err != nil {
+			log.Error("[ACI API] Read pubkey file failed: %v", err.Error())
+
+			result, _ := json.Marshal(map[string]string{"message": "Get pubkey file failed"})
+			return http.StatusNotFound, result
+		} 	 		
+    } else {
+		log.Error("[ACI API] No pubkey file")
+
+		result, _ := json.Marshal(map[string]string{"message": "No pubkey file"})
+		return http.StatusNotFound, result    	
+    }
 	return http.StatusOK, pubkey
 }
 
 func GetACIHandler(ctx *macaron.Context, log *logs.BeeLogger) (int, []byte) {
-	var img []byte
+	namespace := ctx.Params(":namespace")
+	acifilename := ctx.Params(":acifile")
+
+    nameTemp := strings.Trim(acifilename, ".asc")
+	imgname := strings.Trim(nameTemp, ".aci")
+
 	var err error
+    aci := &models.AciDetail{}
 
-	name := ctx.Params(":acname")
+	r := new(models.AciRepository)
+	if aci, err = r.GetAciByName(namespace, imgname); err != nil {
+		log.Error("[ACI API] Get aci %v details failed: %v", namespace, err.Error())
 
-	//support to fetch images from location storage, it will be supported to fetch from cloud etc.
-	imgpath := setting.ImagePath + "/acpool/" + name
-	if img, err = ioutil.ReadFile(imgpath); err != nil {
-		// TBD: consider to fetch image from other storage medium
+		result, _ := json.Marshal(map[string]string{"message": "Get aci details failed"})
+		return http.StatusNotFound, result
+	}
 
-		log.Error("[ACI API] Get ACI file failed: %v", err.Error())
+    var imgpath string
+	if b := strings.Contains(acifilename, ".asc"); b == true {
+		imgpath = aci.SignPath
+	} else {
+		imgpath = aci.AciPath
+	}
+    
+	img, err := ioutil.ReadFile(imgpath)
+	if err != nil {
+		log.Error("[ACI API] Read ACI file failed: %v", err.Error())
+
 		result, _ := json.Marshal(map[string]string{"message": "Get ACI file failed"})
 		return http.StatusNotFound, result
 	}
 
 	return http.StatusOK, img
-
 }
