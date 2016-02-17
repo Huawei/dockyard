ce38f257af7cec54e63ff238cbb0bb0642026936
MabinGo <bin.ma@huawei.com>
2016-02-18 01:25:51 +0800

    Add driver support for the newest API of upyun

--- backend/backend.go
+++ backend/backend.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/containerops/dockyard/backend/drivers/oss"
 	_ "github.com/containerops/dockyard/backend/drivers/qcloud"
 	_ "github.com/containerops/dockyard/backend/drivers/qiniu"
+	_ "github.com/containerops/dockyard/backend/drivers/upyun"
 	"github.com/containerops/wrench/setting"
 )
 
