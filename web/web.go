b20ef9fc82e7813b73d1878b03cedaefe097317d
Quanyi Ma <maquanyi@huawei.com>
2016-04-03 03:01:39 +0800

    Remove models => model

--- web/web.go
+++ web/web.go
@@ -1,47 +1,16 @@
 package web
 
 import (
-	"fmt"
-	"strings"
-
 	"gopkg.in/macaron.v1"
 
-	"github.com/containerops/dockyard/backend"
 	"github.com/containerops/dockyard/middleware"
-	"github.com/containerops/dockyard/models"
-	"github.com/containerops/dockyard/oss"
 	"github.com/containerops/dockyard/router"
-	"github.com/containerops/dockyard/setting"
-	"github.com/containerops/dockyard/utils/db"
 )
 
 func SetDockyardMacaron(m *macaron.Macaron) {
-	if err := db.RegisterDriver(setting.DBDriver); err != nil {
-		fmt.Printf("Register database driver error: %s\n", err.Error())
-	} else {
-		db.Drv.RegisterModel(new(models.Repository), new(models.Tag), new(models.Image))
-		err := db.Drv.InitDB(setting.DBDriver, setting.DBUser, setting.DBPasswd, setting.DBURI, setting.DBName, setting.DBDB)
-		if err != nil {
-			fmt.Printf("Connect database error: %s\n", err.Error())
-		}
-	}
-
-	backend.InitBackend()
-
-	if err := middleware.Initfunc(); err != nil {
-		fmt.Printf("Init middleware error: %s\n", err.Error())
-	}
-
 	//Setting Middleware
 	middleware.SetMiddlewares(m)
 
 	//Setting Router
 	router.SetRouters(m)
-
-	//Start Object Storage Service if sets in conf
-	if strings.EqualFold(setting.OssSwitch, "enable") {
-		ossobj := oss.Instance()
-		ossobj.StartOSS()
-	}
-
 }
