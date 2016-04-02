660339ae086b17c32d6cd5f9c950dfbf7b5b6a6e
Meaglith Ma <genedna@gmail.com>
2016-04-03 03:19:19 +0800

    Add web index output in handler and router

---
+++ handler/webv1.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"gopkg.in/macaron.v1"
+)
+
+func IndexV1Handler(ctx *macaron.Context) (int, []byte) {
+	result, _ := json.Marshal(map[string]string{"message": "Wenduer Backend REST API Service"})
+	return http.StatusOK, result
+}
--- router/router.go
+++ router/router.go
@@ -2,8 +2,10 @@ package router
 
 import (
 	"gopkg.in/macaron.v1"
+
+	"github.com/containerops/dockyard/handler"
 )
 
 func SetRouters(m *macaron.Macaron) {
-
+	m.Get("/", handler.IndexV1Handler)
 }
