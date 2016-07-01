f32756b52c01e1db64b2174f1fde229cd5288603
Meaglith Ma <genedna@gmail.com>
2016-07-01 21:54:58 +0800

    Add Docker Registry V1 and V2 router and handler function definition.

--- router/router.go
+++ router/router.go
@@ -26,8 +26,42 @@ func SetRouters(m *macaron.Macaron) {
 	m.Get("/", handler.IndexV1Handler)
 
 	//Docker Registry V1
+	m.Group("/v1", func() {
+		m.Get("/_ping", handler.GetPingV1Handler)
+
+		m.Get("/users", handler.GetUsersV1Handler)
+		m.Post("/users", handler.PostUsersV1Handler)
+
+		m.Group("/repositories", func() {
+			m.Put("/:namespace/:repository/tags/:tag", handler.PutTagV1Handler)
+			m.Put("/:namespace/:repository/images", handler.PutRepositoryImagesV1Handler)
+			m.Get("/:namespace/:repository/images", handler.GetRepositoryImagesV1Handler)
+			m.Get("/:namespace/:repository/tags", handler.GetTagV1Handler)
+			m.Put("/:namespace/:repository", handler.PutRepositoryV1Handler)
+		})
+
+		m.Group("/images", func() {
+			m.Get("/:image/ancestry", handler.GetImageAncestryV1Handler)
+			m.Get("/:image/json", handler.GetImageJSONV1Handler)
+			m.Get("/:image/layer", handler.GetImageLayerV1Handler)
+			m.Put("/:image/json", handler.PutImageJSONV1Handler)
+			m.Put("/:image/layer", handler.PutImageLayerV1Handler)
+			m.Put("/:image/checksum", handler.PutImageChecksumV1Handler)
+		})
+	})
 
 	//Docker Registry V2
+	m.Group("/v2", func() {
+		m.Get("/", handler.GetPingV2Handler)
+		m.Head("/:namespace/:repository/blobs/:digest", handler.HeadBlobsV2Handler)
+		m.Post("/:namespace/:repository/blobs/uploads", handler.PostBlobsV2Handler)
+		m.Patch("/:namespace/:repository/blobs/uploads/:uuid", handler.PatchBlobsV2Handler)
+		m.Put("/:namespace/:repository/blobs/uploads/:uuid", handler.PutBlobsV2Handler)
+		m.Get("/:namespace/:repository/blobs/:digest", handler.GetBlobsV2Handler)
+		m.Put("/:namespace/:repository/manifests/:tag", handler.PutManifestsV2Handler)
+		m.Get("/:namespace/:repository/tags/list", handler.GetTagsListV2Handler)
+		m.Get("/:namespace/:repository/manifests/:tag", handler.GetManifestsV2Handler)
+	})
 
 	//Appc Discovery
 	m.Group("/appc", func() {
