ad0486aaf74a6f5ac9e5a93e774e58e3f73a11f1
Quanyi Ma <maquanyi@huawei.com>
2016-07-31 06:42:10 -0700

    Complete the PutRepositoryV1Handler function.

--- handler/dockerv1.go
+++ handler/dockerv1.go
@@ -18,9 +18,14 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/ngaut/log"
 	"gopkg.in/macaron.v1"
+
+	"github.com/containerops/dockyard/models"
+	"github.com/containerops/dockyard/utils"
 )
 
 //GetPingV1Handler returns http.StatusOK(200) when Dockyard provide the Docker Registry V1 support.
@@ -70,8 +75,61 @@ func GetTagV1Handler(ctx *macaron.Context) (int, []byte) {
 	return http.StatusOK, result
 }
 
-//PutRepositoryV1Handler
+//PutRepositoryV1Handler will create or update the repository, it's first step of Docker push.
+//TODO: @1 When someone create or update the repository, it will be locked to forbidden others action include pull action.
+//TODO: @2 Add a config option for allow/forbidden Docker client pull action when a repository is locked.
+//TODO: @3 Intergated with [Crew](https://github.com/containerops/crew).
+//TODO: @4 Token will be store in Redis, and link the push action with username@repository.
 func PutRepositoryV1Handler(ctx *macaron.Context) (int, []byte) {
+	var username, body string
+	//var passwd string
+	var err error
+
+	if username, _, err = utils.DecodeBasicAuth(ctx.Req.Header.Get("Authorization")); err != nil {
+		log.Errorf("[%s] decode Authorization error: %s", ctx.Req.RequestURI, err.Error())
+
+		result, _ := json.Marshal(map[string]string{"Error": "Decode Authorization Error"})
+		return http.StatusUnauthorized, result
+	}
+
+	//When integrated with crew, like this:
+	//@1: username, passwd, _ := utils.DecodeBasicAuth(ctx.Req.Header.Get("Authorization"))
+	//@2: username, passwd authorizated in Crew.
+
+	namespace := ctx.Params(":namespace")
+	repository := ctx.Params(":repository")
+
+	//When integrated the Crew, should be check the privilage.
+	if username != namespace {
+
+	}
+
+	//In Docker Registry V1, the repository json data in the body of `PUT /v1/:namespace/:repository`
+	if body, err = ctx.Req.Body().String(); err != nil {
+		log.Errorf("[%s] get repository json from http body error: %s", ctx.Req.RequestURI, err.Error())
+
+		result, _ := json.Marshal(map[string]string{"Error": "Get Repository JSON Error"})
+		return http.StatusBadRequest, result
+	}
+
+	//Create or update the repository.
+	r := new(models.DockerV1)
+	if e := r.Put(namespace, repository, body, ctx.Req.Header.Get("User-Agent")); e != nil {
+		log.Errorf("[%s] put repository error: %s", ctx.Req.RequestURI, e.Error())
+
+		result, _ := json.Marshal(map[string]string{"Error": "PUT Repository Error"})
+		return http.StatusBadRequest, result
+	}
+
+	//If the Docker client use "X-Docker-Token", will return a randon token value.
+	if ctx.Req.Header.Get("X-Docker-Token") == "true" {
+		token := fmt.Sprintf("Token signature=%v,repository=\"%v/%v\",access=%v",
+			utils.MD5(username), namespace, repository, "write")
+
+		ctx.Resp.Header().Set("X-Docker-Token", token)
+		ctx.Resp.Header().Set("WWW-Authenticate", token)
+	}
+
 	result, _ := json.Marshal(map[string]string{})
 	return http.StatusOK, result
 }
