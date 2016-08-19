b0748515bc1311292b771542dc36d0973fa035e0
liang chenye <liangchenye@huawei.com>
2016-08-19 09:45:37 +0800

    add travis to do 'unittest/go vet/golint' checks
    Signed-off-by: liang chenye <liangchenye@huawei.com>

--- tests/web_test.go
+++ tests/web_test.go
@@ -1,23 +1,39 @@
 package tests
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 	"testing"
 )
 
+var (
+	testServer = ""
+)
+
+func init() {
+	// Start a dockyard web server and set the enviornment like this:
+	//     $ export US_TEST_SERVER=https://containerops.me
+	testServer = os.Getenv("US_TEST_SERVER")
+}
+
 func Test_IndexHandler(t *testing.T) {
-	endpoint := "https://containerops.me"
+	if testServer == "" {
+		fmt.Println("Skip index handler testing since 'US_TEST_SERVER' is not set")
+		return
+	}
+	endpoint := testServer
 
 	resp, err := http.Get(endpoint)
-	defer resp.Body.Close()
-
 	if err != nil {
 		t.Errorf("Test REST API \"/\" Error: %s .", err.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Test REST API \"/\" Error StatusCode: %d .", resp.StatusCode)
 	} else {
-		if resp.StatusCode != http.StatusOK {
-			t.Errorf("Test REST API \"/\" Error StatusCode: %d .", resp.StatusCode)
-		} else {
-			t.Log("Test REST API \"/\" Successfully.")
-		}
+		t.Log("Test REST API \"/\" Successfully.")
 	}
 }
