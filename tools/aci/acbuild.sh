999d2e1bbc2a2fb43c661bf10dadbe5de4d4fea7
Quanyi Ma <maquanyi@huawei.com>
2016-08-19 23:20:23 -0400

    Update the gpg file and keys

---
+++ tools/aci/acbuild.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -e
+
+# Build and statically link acserver (this requires it to already be downloaded)
+echo "Building Dockyard ACI File..."
+CGO_ENABLED=0 GOOS=linux go build -o dockyard -a -tags netgo -ldflags '-w' github.com/containerops/dockyard
+
+rmDockyard() { 
+  rm dockyard; 
+}
+
+acbuildEnd() {
+  rmDockyard
+  export EXIT=$?
+  acbuild --debug end && exit $EXIT
+}
+
+# When the script exits, remove the binary we built
+trap rmDockyard EXIT
+
+# Start the build with an empty ACI
+acbuild --debug begin
+
+# In the event of the script exiting, end the build
+trap acbuildEnd EXIT
+
+# Name the ACI
+acbuild --debug set-name dockyard.sh/genedna/dockyard 
+
+# Copy the binary and its templates into the ACI
+acbuild --debug copy dockyard /dockyard
+acbuild --debug copy $GOPATH/src/github.com/containerops/dockyard/conf /conf
+acbuild --debug copy $GOPATH/src/github.com/containerops/dockyard/cert /cert
+acbuild --debug copy $GOPATH/src/github.com/containerops/dockyard/data /data
+acbuild --debug copy $GOPATH/src/github.com/containerops/dockyard/external /external
+acbuild --debug copy $GOPATH/src/github.com/containerops/dockyard/log /log
+acbuild --debug copy $GOPATH/src/github.com/containerops/dockyard/views /views
+
+# Add a mount point for the ACIs to serve
+acbuild --debug mount add acis /acis
+
+# Run acserver
+acbuild --debug set-exec -- /acserver -port 443 localhost 
+
+# Save the resulting ACI
+acbuild --debug write --overwrite dockyard-latest-linux-amd64.aci
