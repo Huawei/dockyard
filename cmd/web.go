52b56d9bdf4c2b594ee21a5625b33693936f5951
Meaglith Ma <genedna@gmail.com>
2016-09-25 14:59:54 +0800

    Refactoring the web with cobra.

--- cmd/web.go
+++ cmd/web.go
@@ -17,7 +17,18 @@ limitations under the License.
 package cmd
 
 import (
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+
 	"github.com/spf13/cobra"
+	"gopkg.in/macaron.v1"
+
+	"github.com/containerops/configure"
+	"github.com/containerops/dockyard/utils"
+	"github.com/containerops/dockyard/web"
 )
 
 var address string
@@ -40,5 +51,46 @@ func init() {
 }
 
 func runWeb(cmd *cobra.Command, args []string) {
+	m := macaron.New()
 
+	//Set Macaron Web Middleware And Routers
+	web.SetDockyardMacaron(m)
+
+	listenMode := configure.GetString("listenmode")
+	switch listenMode {
+	case "http":
+		listenaddr := fmt.Sprintf("%s:%d", address, port)
+		if err := http.ListenAndServe(listenaddr, m); err != nil {
+			fmt.Printf("Start Dockyard http service error: %v\n", err.Error())
+
+		}
+		break
+	case "https":
+		listenaddr := fmt.Sprintf("%s:443", address)
+		server := &http.Server{Addr: listenaddr, TLSConfig: &tls.Config{MinVersion: tls.VersionTLS10}, Handler: m}
+		if err := server.ListenAndServeTLS(configure.GetString("httpscertfile"), configure.GetString("httpskeyfile")); err != nil {
+			fmt.Printf("Start Dockyard https service error: %v\n", err.Error())
+
+		}
+		break
+	case "unix":
+		listenaddr := fmt.Sprintf("%s", address)
+		if utils.IsFileExist(listenaddr) {
+			os.Remove(listenaddr)
+		}
+
+		if listener, err := net.Listen("unix", listenaddr); err != nil {
+			fmt.Printf("Start Dockyard unix socket error: %v\n", err.Error())
+
+		} else {
+			server := &http.Server{Handler: m}
+			if err := server.Serve(listener); err != nil {
+				fmt.Printf("Start Dockyard unix socket error: %v\n", err.Error())
+
+			}
+		}
+		break
+	default:
+		break
+	}
 }
