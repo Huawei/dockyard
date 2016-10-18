4ef3f539f135b88a219a67bd84b6df5412c26099
Quanyi Ma <maquanyi@huawei.com>
2016-10-19 02:53:04 +0800

    Fix the package path.
    Signed-off-by: Meaglith Ma <genedna@gmail.com>

--- web/web.go
+++ web/web.go
@@ -19,8 +19,8 @@ package web
 import (
 	"gopkg.in/macaron.v1"
 
-	"github.com/containerops/dockyard/middleware"
-	"github.com/containerops/dockyard/router"
+	"github.com/huawei/dockyard/middleware"
+	"github.com/huawei/dockyard/router"
 )
 
 func SetDockyardMacaron(m *macaron.Macaron) {
